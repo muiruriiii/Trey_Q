9c1d4d7ea3f2f0673c75239bbb0fc46ad61416b5
muiruriiii <muiruricynthiaaa@gmail.com>
2023-11-23 23:08:39 +0300

    Bottom Navigation View Implemented

--- app/src/main/java/com/android/waterqualitymonitoring/MainActivity.kt
+++ app/src/main/java/com/android/waterqualitymonitoring/MainActivity.kt
@@ -2,10 +2,73 @@ package com.android.waterqualitymonitoring
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.android.waterqualitymonitoring.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout,binding.toolbar, R.string.nav_open, R.string.nav_close )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+        binding.bottomNavigation.background = null
+        binding.bottomNavigation.setOnItemSelectedListener {
+            item -> when(item.itemId){
+                R.id.bottom_account ->openFragment(AccountFragment())
+                R.id.bottom_notifications ->openFragment(NotificationsFragment())
+                R.id.bottom_history ->openFragment(HistoryFragment())
+            }
+            true
+        }
+        fragmentManager = supportFragmentManager
+        openFragment(NotificationsFragment())
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.account -> openFragment(AccountFragment())
+            R.id.notifications -> openFragment(NotificationsFragment())
+            R.id.history -> openFragment(HistoryFragment())
+            R.id.account -> Toast.makeText(this, "Account", Toast.LENGTH_SHORT).show()
+            R.id.notifications -> Toast.makeText(this, "Notifications", Toast.LENGTH_SHORT).show()
+            R.id.history -> Toast.makeText(this, "History", Toast.LENGTH_SHORT).show()
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.getOnBackPressedDispatcher().onBackPressed()
+        }
+    }
+    private fun openFragment(fragment: Fragment){
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
