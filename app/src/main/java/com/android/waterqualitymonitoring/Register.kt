e07c4710eac65e2792cfb080ef9b76cc2b859741
muiruriiii <muiruricynthiaaa@gmail.com>
2023-11-27 16:53:51 +0300

    Initial commit

---
+++ app/src/main/java/com/android/waterqualitymonitoring/Register.kt
@@ -0,0 +1,13 @@
+package com.android.waterqualitymonitoring
+//import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
